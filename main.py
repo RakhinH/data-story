78a362b333cfe712e700536a255ed199d1369b12
RakhinH <rakhinchowdhury@icloud.com>
2024-11-26 10:48:43 +0000

    ngfngfngf

--- main.py
+++ main.py
@@ -7,19 +7,18 @@ c = conn.cursor()
 # Create tables
 c.execute('''CREATE TABLE IF NOT EXISTS Student_info (
                 student_id INTEGER PRIMARY KEY,
-                teacher_id INTEGER,
+                FOREIGN KEY (teacher_student_id) REFERENCES Teacher_info(teacher_id),
                 student_first TEXT,
                 student_last TEXT,
-                student_contact_info_id INTEGER,
+                FOREIGN KEY (student_contact) REFERENCES Student_contact_info(student_contact_info_id),
                 student_attendance INTEGER,
                 student_course TEXT,
-                FOREIGN KEY(student_timetable) REFERENCES Timetable(id),
                 
              )''')
 
 c.execute('''CREATE TABLE IF NOT EXISTS Student_contact_info (
                 student_contact_info_id INTEGER PRIMARY KEY,
-                student_email_address TEXT,
+                student_email_address TEXT, 
                 student_house_address TEXT,
                 student_number INTEGER,
                 student_nickname TEXT,
@@ -27,11 +26,10 @@ c.execute('''CREATE TABLE IF NOT EXISTS Student_contact_info (
              )''') 
 
 c.execute('''CREATE TABLE IF NOT EXISTS Parent_info (
-                parent_id INTEGER PRIMARY KEY, 
-                student_id  INTEGER,
+                parent_id INTEGER PRIMARY KEY,
                 parent_first TEXT,
                 parent_last TEXT,
-                parent_contact_info_id INTEGER,
+                FOREIGN KEY (parent_contact) REFERENCES Parent_contact_info(parent_contact_info_id),
              )''')
 
 c.execute('''CREATE TABLE IF NOT EXISTS Parent_contact_info (
@@ -43,22 +41,24 @@ c.execute('''CREATE TABLE IF NOT EXISTS Parent_contact_info (
              )''')
 
 c.execute('''CREATE TABLE IF NOT EXISTS Teacher_info (
-                
+               PRIMARY KEY teacher_id INTEGER,
+               FOREIGN KEY (teacher_contact) REFERENCES Teacher_contact_info(teacher_contact_info_id),
+               teacher_qualification TEXT,
+               teacher_first TEXT,
+               teacher_last TEXT,
              )''')   
 
 c.execute('''CREATE TABLE IF NOT EXISTS Teacher_contact_info (
-                
-             )''')               
+               PRIMARY KEY teacher_contact_info_id INTEGER,
+               teacher_number INTEGER,
+               teacher_email_address TEXT,
+               teacher_nickname TEXT,
+               teacher_house_address TEXT,
+             )''') 
 
-c.execute('''CREATE TABLE IF NOT EXISTS Timetable (
+c.execute("INSERT INTO Student_info VALUES (, 1, 'John', 'Doe', 1, 'Math', 90)")            
                 
-             )''')
-c.execute('''CREATE TABLE IF NOT EXISTS Books (
-                id INTEGER PRIMARY KEY,
-                title TEXT,
-                author_id INTEGER,
-                FOREIGN KEY(author_id) REFERENCES Authors(id)
-             )''')
+
 
 
 
